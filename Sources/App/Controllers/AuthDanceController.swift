8f3046898d738d236964bf1603426bc2e8bbc905
Igor <iandrosov73@gmail.com>
2018-11-30 07:43:10 -0500

    GEt Salesorce autoriziatiopndoe adn sen request for session token

--- Sources/App/Controllers/AuthDanceController.swift
+++ Sources/App/Controllers/AuthDanceController.swift
@@ -8,6 +8,19 @@
 import Vapor
 
 final class AuthDanceController {
+    
+    struct SFTokenRequest: Content {
+        var grant_type: String
+        var client_id: String
+        var client_secret: String
+        var redirect_uri: String
+        var code: String
+    }
+    struct SFAuthCode: Content {
+        var display: String?
+        var code: String?
+    }
+    
     // construct OAuth URL with parameters and strat auth flow
     func sfauth(_ req: Request) throws -> Future<Response> {
         //return try req.make("hello")
@@ -39,48 +52,40 @@ final class AuthDanceController {
     func sfcallback(_ req: Request) throws -> String {
         print("### TEST: ")
         print(req)
+        let flags = try req.query.decode(SFAuthCode.self)
+        let authCode = flags.code
+        print("### CODE: ")
+        print(authCode!) // Flags
+        
+        
         // Assemble call to get access token
         let urlString = "https://login.salesforce.com/services/oauth2/token"
         var headers = HTTPHeaders()
         headers.add(name: "Content-Type", value: "application/json")
         
-        //let drop = Droplet()
-        //let apiResponse = try drop.client.get("https://api.com", query: ["q": queryString])
+        let tokenRequest = SFTokenRequest(grant_type: "authorization_code",
+        client_id: "3MVG9yZ.WNe6byQDinV4pEtYbk.XKrK3LwCNZtKCJ9lKnd6keoaNjuNXu7i3EBK_lLzNSZnXAkQE.2gw4xFZn",
+        client_secret: "8219049706333485472",
+        redirect_uri: "https://localhost:8080/authresult",
+        code: authCode!)
+        let res = try req.client().post(urlString) { loginReq in
+            // encode the loginRequest before sending
+            try loginReq.content.encode(tokenRequest)
+        }
+        print("### RESPONSE: ")
+        print(res) // Future<Response>
         
         // Connect a new client to the supplied hostname.
         //let client = try req.client()
-        let client = try HTTPClient.connect(hostname: urlString, on: ...).wait()
-        print(client) // HTTPClient
+        //let client = try HTTPClient.connect(hostname: urlString, on: ...).wait()
+        //print(client) // HTTPClient
         
         // Create an HTTP request: GET /
-        let httpReq = HTTPRequest(method: .POST, url: urlString)
+        //let httpReq = HTTPRequest(method: .POST, url: urlString)
         
         // Send the HTTP request, fetching a response
-        let httpRes = try client.send(httpReq).wait()
-        print(httpRes) // HTTPResponse
-        
-        /**
-        let loginRequest = ["grant_type" : "authorization_code",
-    "client_id" : "3MVG9yZ.WNe6byQDinV4pEtYbk.XKrK3LwCNZtKCJ9lKnd6keoaNjuNXu7i3EBK_lLzNSZnXAkQE.2gw4xFZn",
-            "client_secret" : "",
-            "redirect_uri" : "",
-            "code" : ""]
-        
-        return try req.client().post(urlString, headers: headers, beforeSend: { loginReq in
-            
-            // Encode Content before Request is sent
-            return try loginReq.content.encode(loginRequest)
-        
-        }).map(to: HTTPResponseStatus.self) { response in
-    
-            let decoded = try response.content.syncDecode(APIAccessResponse.self)
-    
-            print("token:\(decoded.token)")
-    
-            //return LoginSession(userID: user.userID, userToken: UUID().uuidString, accessToken: decoded.token)
-    
-        }
-    **/
+        //let httpRes = try client.send(httpReq).wait()
+        //print(httpRes) // HTTPResponse
         
         return "Hello, Salesforce OAuth!"
     }
