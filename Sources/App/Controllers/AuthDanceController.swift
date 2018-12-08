4111587e3e508747436dedcc6cb818d72eb542ea
Igor <iandrosov73@gmail.com>
2018-12-07 20:07:11 -0500

    oauth update

--- Sources/App/Controllers/AuthDanceController.swift
+++ Sources/App/Controllers/AuthDanceController.swift
@@ -24,14 +24,6 @@ final class AuthDanceController {
     // construct OAuth URL with parameters and strat auth flow
     func sfauth(_ req: Request) throws -> Future<Response> {
         //return try req.make("hello")
-        // Connect a new client to the supplied hostname.
-        //let client = try HTTPClient.connect(hostname: "vapor.codes", on: ...).wait()
-        //print(client) // HTTPClient
-        // Create an HTTP request: GET /
-        //let httpReq = HTTPRequest(method: .GET, url: "https://login.salesforce.com/services/oauth2/authorize")
-        // Send the HTTP request, fetching a response
-        //let httpRes = try client.send(httpReq).wait()
-        //print(httpRes) // HTTPResponse
 
         //let httpReq = Request(http: .GET, using: "https://login.salesforce.com/services/oauth2/authorize")
         let myurl : String = "https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG9yZ.WNe6byQDinV4pEtYbk.XKrK3LwCNZtKCJ9lKnd6keoaNjuNXu7i3EBK_lLzNSZnXAkQE.2gw4xFZn&redirect_uri=https://localhost:8080/authresult&display=page"
